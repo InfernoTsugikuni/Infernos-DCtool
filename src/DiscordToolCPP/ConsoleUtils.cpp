498bb5521ae1d43ce9ba6127a8d7d2944e4a9d69
Inferno <167332139+InfernoTsugikuni@users.noreply.github.com>
2025-04-27 21:03:00 +0200

    Delete src directory

--- src/DiscordToolCPP/ConsoleUtils.cpp
+++
@@ -1,6 +0,0 @@
-#include "ConsoleUtils.h"
-
-void SetConsoleColor(WORD color) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, color);
-}
--- src/DiscordToolCPP/ConsoleUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef CONSOLE_UTILS_H
-#define CONSOLE_UTILS_H
-
-#include <windows.h>
-
-void SetConsoleColor(WORD color);
-
-#endif // CONSOLE_UTILS_H
